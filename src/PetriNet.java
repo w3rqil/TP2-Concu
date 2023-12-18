882aa07a2dcec676de8bbb4d187bc611f32a0dcd
w3rqil <128259227+w3rqil@users.noreply.github.com>
2023-12-17 22:11:17 -0300

    Update PetriNet.java

--- src/PetriNet.java
+++ src/PetriNet.java
@@ -123,7 +123,7 @@ public class PetriNet {
     private final double[] initialMarking = {1, 1, 1, 0, 3, 0, 0, 1, 1, 0, 2, 0, 0, 0, 1, 0, 0, 0, 1};
 
     //                               0  1  2  3  4   5  6   7   8  9   10 11 12 13
-    private final double[] aTimes = { 0, 0, 5, 5, 0, 0, 10, 10, 0, 10, 10, 0, 0, 5};
+    private final double[] aTimes = { 0, 0, 5, 5, 0, 0, 10, 10, 0, 0, 10, 10, 0, 5};
     public List<Integer> tInvariantSum;
 
     private List<List<Integer>> tInvariants;
